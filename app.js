8a8a42e0bdff9491c7327ffbc561db5821c797ea
yashviradiaofficial <yashviradiaofficial@gmail.com>
2021-05-27 19:38:40 +0200

    to do list first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+
+const app = express();
+
+const items = ["Hallo, was wirst du erledigen?"];
+const workItems = [];
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+
+  const day = date.getDate();
+
+  res.render("list", {
+    listTitle: day, newListItems: items
+  });
+
+
+});
+
+app.post("/", function(req, res) {
+
+
+  const item = req.body.newItem;
+
+  console.log(req.body);
+
+
+  if(req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems });
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
+})
+
+app.listen(3000, function() {
+  console.log("Server is running on Port 3000");
+});
